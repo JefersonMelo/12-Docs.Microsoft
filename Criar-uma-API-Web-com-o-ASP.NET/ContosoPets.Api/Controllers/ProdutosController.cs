49a7685472ef6bc1ff6d66ab1b78142300fe6f9d
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-12 18:32:51 -0300

    commit Exercício – Adicionar um controlador

---
+++ Criar-uma-API-Web-com-o-ASP.NET/ContosoPets.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContosoPets.Api.Data;
+using ContosoPets.Api.Models;
+
+namespace ContosoPets.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ContosoPetsContext _context;
+
+        public ProdutosController(ContosoPetsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Produto>> GetAll( ) =>
+            _context.Produtos.ToList();
+
+        // GET by ID action
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
